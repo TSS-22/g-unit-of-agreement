b20db8c69d6ccdf36f89ab7c803b6341ecf21b81
TSS-22 <andpartyandbullshit@protonmail.com>
2022-07-24 18:29:58 +1200

    done the entropy gradient descent
    URGENT NEED FOR OPTIMSATION

--- g_unit_aggr.py
+++ g_unit_aggr.py
@@ -1,23 +1,32 @@
 import numpy as np
 
-def standard_norm_dist(arg_stdDev, arg_precision):
-    x = np.linspace(-arg_stdDev,arg_stdDev,arg_precision)
+# Shanon entropy, +0.1 is addedd to prevent the error of divided by zero
+def entropy_info(arg_matrix):
+    return np.sum(np.power(arg_matrix,2)*np.log(np.power(arg_matrix,2)+0.1))
+
+def standard_norm_dist(arg_widthDistri, arg_resolution):
+    x = np.linspace(arg_widthDistri,
+        arg_widthDistri,
+        arg_resolution)
     prob_density = np.exp(-0.5*(x)**2)/np.sqrt(2*np.pi)
     return prob_density
 
-def range_values(x, FACTOR_STD, arg_precision):
-    return np.linspace(x-FACTOR_STD,x+FACTOR_STD,arg_precision)
+def range_values(x, FACTOR_STD, arg_resolution):
+    return np.linspace(x-FACTOR_STD,
+        x+FACTOR_STD,
+        arg_resolution)
     
 def area2distri(val1,val2,stdDistri):
-    return np.sum(np.fmin(stdDistri[val1<=np.max(val2)] ,stdDistri[val2>=np.min(val1)]))/np.sum(stdDistri)
+    return np.sum(np.fmin(stdDistri[val1<=np.max(val2)] ,
+        stdDistri[val2>=np.min(val1)]))/np.sum(stdDistri)
 
 # arg_values must be a mtrix array, with row = occurences, col = variables
-def g_unit_aggr(arg_values,arg_precision=100, arg_stdDev=1, arg_widthDistri=3.5):  
+def g_unit_aggr(arg_values,arg_resolution=100, arg_stdDev=1.0, arg_widthDistri=3.5):  
     # Create the standard deviation used factor used for aggreement computation
     std_range = np.std(arg_values)*arg_stdDev
 
     # Create the corresponding normal distribution density function
-    stdDistri = standard_norm_dist(arg_widthDistri, arg_precision)
+    stdDistri = standard_norm_dist(arg_widthDistri, arg_resolution)
 
     # Init counter
     xIdxRow = 0
@@ -35,8 +44,8 @@ def g_unit_aggr(arg_values,arg_precision=100, arg_stdDev=1, arg_widthDistri=3.5)
         while xIdxCol < arg_values.shape[1]:
             while yIdxRow < arg_values.shape[0]:
                 while yIdxCol < arg_values.shape[1]:
-                    x_range = range_values(arg_values[xIdxRow,yIdxCol], std_range, arg_precision)
-                    y_range = range_values(arg_values[yIdxRow,yIdxCol], std_range, arg_precision)
+                    x_range = range_values(arg_values[xIdxRow,yIdxCol], std_range, arg_resolution)
+                    y_range = range_values(arg_values[yIdxRow,yIdxCol], std_range, arg_resolution)
                     
                     if arg_values[xIdxRow,yIdxCol] <= arg_values[yIdxRow,yIdxCol]:
                         temp_aggr[yIdxCol] = area2distri(y_range,x_range,stdDistri)
@@ -56,8 +65,25 @@ def g_unit_aggr(arg_values,arg_precision=100, arg_stdDev=1, arg_widthDistri=3.5)
 
     return G_matrix
 
+def g_unit_aggr_auto(arg_values,arg_resolution=100, arg_stdDev=1, arg_widthDistri=3.5, arg_learning_rate=0.05):
+    
+    ntrp_g_matrix_n = entropy_info(g_unit_aggr(arg_values,arg_resolution, arg_stdDev, arg_widthDistri))
+    ntrp_g_matrix_n1 = entropy_info(g_unit_aggr(arg_values, arg_resolution, arg_stdDev+arg_learning_rate, arg_widthDistri))
 
-       
-        
-
-        
+    if ntrp_g_matrix_n>ntrp_g_matrix_n1:
+        arg_stdDev=arg_stdDev-arg_learning_rate
+        ntrp_g_matrix_n1 = entropy_info(g_unit_aggr(arg_values, arg_resolution, arg_stdDev, arg_widthDistri))
+        if ntrp_g_matrix_n>ntrp_g_matrix_n1:
+            return g_unit_aggr(arg_values, arg_resolution, arg_stdDev, arg_widthDistri)
+        else:
+            while True:
+                arg_stdDev=arg_stdDev-arg_learning_rate
+                ntrp_g_matrix_n1 = entropy_info(g_unit_aggr(arg_values, arg_resolution, arg_stdDev, arg_widthDistri))
+                if ntrp_g_matrix_n>=ntrp_g_matrix_n1:
+                    return g_unit_aggr(arg_values, arg_resolution, arg_stdDev+arg_learning_rate, arg_widthDistri)
+    else:
+        while True:
+            arg_stdDev=arg_stdDev+arg_learning_rate
+            ntrp_g_matrix_n1 = entropy_info(g_unit_aggr(arg_values, arg_resolution, arg_widthDistri, arg_stdDev))
+            if ntrp_g_matrix_n>=ntrp_g_matrix_n1:
+                return g_unit_aggr(arg_values, arg_resolution, arg_stdDev-arg_learning_rate, arg_widthDistri)
