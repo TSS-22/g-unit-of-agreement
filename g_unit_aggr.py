26588bcff69792770c25cc5a5d54c68c07fd4dd4
TSS-22 <andpartyandbullshit@protonmail.com>
2022-07-25 09:16:00 +1200

    added debug function

--- g_unit_aggr.py
+++ g_unit_aggr.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 # Shanon entropy, +0.1 is addedd to prevent the error of divided by zero
 def entropy_info(arg_matrix):
@@ -94,3 +95,21 @@ def g_unit_aggr_auto(arg_values,arg_resolution=100, arg_stdDev=1, arg_widthDistr
             if ntrp_g_matrix_n>=ntrp_g_matrix_n1:
                 return g_unit_aggr(arg_values, arg_resolution, arg_stdDev-arg_learning_rate, arg_widthDistri)
 
+
+
+
+# DEBUG FUNCTION TO GET THE RANGE MATRIX
+def range_matrix(arg_values,arg_resolution=100, arg_stdDev=1.0, arg_widthDistri=3.5):  
+    # Create the standard deviation used factor used for aggreement computation
+    std_range = np.std(arg_values)*arg_stdDev
+
+    range_matrix = np.zeros([arg_values.shape[0],arg_values.shape[1]], np.ndarray)
+    xIdxRow=0
+    while xIdxRow < arg_values.shape[0]:
+        yIdxCol=0
+        while yIdxCol < arg_values.shape[1]:
+            range_matrix[xIdxRow, yIdxCol] = range_values(arg_values[xIdxRow, yIdxCol], std_range, arg_resolution)
+            yIdxCol=yIdxCol+1
+        xIdxRow=xIdxRow+1
+
+    return range_matrix
