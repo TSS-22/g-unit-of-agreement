d4e9289bc2c14265a851586643f389baf4de8649
TSS-22 <andpartyandbullshit@protonmail.com>
2022-08-08 09:31:20 +1200

    opti a bit

--- Rust/g_oxy/src/g_unit_aggr.rs
+++ Rust/g_oxy/src/g_unit_aggr.rs
@@ -1,5 +1,5 @@
 use rayon::prelude::*;
-use crate::tools::{area2distri,entropy_info};
+use crate::tools::{area2distri};
 use crate::mat_op::{std_m2df32,mean_m1df32,variance_m2df32, sum_md2f32};
 
 pub fn g_unit_aggr(data: Vec<Vec<f32>>) -> Vec<Vec<f32>>{
@@ -62,30 +62,59 @@ fn compute_g_matrix(data_in: &Vec<Vec<f32>>, load_std: &f32)->Vec<Vec<f32>>{
     return data_out;
 }
 
+fn compute_g_matrix2(data_in: &Vec<Vec<f32>>, load_std: &f32)->Vec<Vec<f32>>{
+    let mut handles = Vec::new();
+    let mut data_out: Vec<Vec<f32>> = Vec::new();
+    
+    handles = data_in.par_iter().map(|row1|{fn_row2(&data_in,&row1,load_std)}).collect();
+
+    return handles;
+}
+
+fn fn_row2(data_in: &Vec<Vec<f32>>, row1: &Vec<f32>, load_std: &f32)->Vec<f32>{
+    let mut handles = Vec::new();
+
+    handles = data_in.par_iter().map(|row2|{compute_area(&row1, &row2, &load_std)}).collect();
+
+    return handles;
+}
+
+fn compute_area(row1: &Vec<f32>, row2: &Vec<f32>, load_std: &f32)->f32{
+    let mut temp_row: Vec<f32> = Vec::new();
+    for (val1, val2) in row1.iter().zip(row2.iter()){
+        if val1.max(*val2)==*val1{
+            temp_row.push(area2distri(*val1, *val2, &load_std));
+        } else {
+            temp_row.push(area2distri(*val2, *val1, &load_std));
+        }
+    }
+    return mean_m1df32(&temp_row);
+}
+
 fn find_opti_g_matrix(data_in: Vec<Vec<f32>>)->Vec<Vec<f32>>{
     let mut load_std:f32 =std_m2df32(&data_in);
     let learning_rate:f32 = 0.05f32;
     let mut opti_g_matrix:Vec<Vec<f32>> = Vec::new();
-    let mut mat_n = compute_g_matrix(&data_in, &load_std);
-    let mut mat_n1 = compute_g_matrix(&data_in, &(load_std+learning_rate));
+    let mut mat_n = compute_g_matrix2(&data_in, &load_std);
+    let mut mat_n1 = compute_g_matrix2(&data_in, &(load_std+learning_rate));
 
-    let mut entropy_n = variance_m2df32(&mat_n)*sum_md2f32(&mat_n);
-    let mut entropy_n1 = variance_m2df32(&mat_n1)*sum_md2f32(&mat_n1);
+    let mut objective_n = variance_m2df32(&mat_n)*sum_md2f32(&mat_n);
+    let mut objective_n1 = variance_m2df32(&mat_n1)*sum_md2f32(&mat_n1);
 
-    if entropy_n > entropy_n1{
-        mat_n1 = compute_g_matrix(&data_in, &(load_std-learning_rate));
-        entropy_n1= variance_m2df32(&mat_n1)*sum_md2f32(&mat_n1);
-        if entropy_n >= entropy_n1{
+    if objective_n > objective_n1{
+        mat_n1 = compute_g_matrix2(&data_in, &(load_std-learning_rate));
+        objective_n1= variance_m2df32(&mat_n1)*sum_md2f32(&mat_n1);
+        if objective_n >= objective_n1{
             opti_g_matrix = mat_n;
         }else{
             load_std = load_std-learning_rate;
             loop{
                 load_std = load_std-learning_rate;
-                entropy_n = entropy_n1;
+                objective_n = objective_n1;
                 mat_n = mat_n1;
-                mat_n1 = compute_g_matrix(&data_in, &load_std);
-                entropy_n1= variance_m2df32(&mat_n1)*sum_md2f32(&mat_n1); 
-                if entropy_n >= entropy_n1 {
+                mat_n1 = compute_g_matrix2(&data_in, &load_std);
+                objective_n1= variance_m2df32(&mat_n1)*sum_md2f32(&mat_n1); 
+                if objective_n >= objective_n1 {
                     opti_g_matrix = mat_n;
                     break;
                 }
@@ -95,11 +124,11 @@ fn find_opti_g_matrix(data_in: Vec<Vec<f32>>)->Vec<Vec<f32>>{
         load_std = load_std+learning_rate;
         loop{
             load_std = load_std+learning_rate;
-            entropy_n = entropy_n1;
+            objective_n = objective_n1;
             mat_n = mat_n1;
-            mat_n1 = compute_g_matrix(&data_in, &load_std);
-            entropy_n1= variance_m2df32(&mat_n1)*sum_md2f32(&mat_n1); 
-            if entropy_n >= entropy_n1 {
+            mat_n1 = compute_g_matrix2(&data_in, &load_std);
+            objective_n1= variance_m2df32(&mat_n1)*sum_md2f32(&mat_n1); 
+            if objective_n >= objective_n1 {
                 opti_g_matrix = mat_n;
                 break;
             }
--- Rust/g_oxy/src/tools.rs
+++ Rust/g_oxy/src/tools.rs
@@ -13,10 +13,8 @@ pub fn area2distri(val1: f32, val2: f32, &width_distri: &f32) -> f32{
     // To account for the small error as we don't take the full logistic distribution to have a more dynamic behavior 
     let error_margin:f32 = 0.0066929f32;
     let val: f32 = (val1-val2)/width_distri;
-        
     if  val < 1f32{
         // Wrong formula
-        //area = (((1f32-(val1-val2)/width_distri).powf(3.2))*1f32.exp())/((1f32-(val1-val2)/width_distri).powf(3.25).exp());
         area = 1f32-(0.5f32+((5f32*val-2.5f32).tanh()/2f32)+((error_margin*2f32*val)-error_margin)); // we transform the logistic cumulative regression to be from [-inf; +inf] to [0;1]  
     }    
     return area as f32;
